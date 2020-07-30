7ac80ef8077708d5cea916e500e6dd950bbb1788
AlyonaMarycheva <alyona.marycheva@mail.ru>
2020-07-30 03:00:32 +0300

    исправлено отображение имени

--- src/components/PlanList/ListOfPlans.js
+++ src/components/PlanList/ListOfPlans.js
@@ -18,14 +18,6 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { useExpService } from '../../context/expService.js';
 import formatService from '../../services/formatService.js';
 
-
-
-const Name = ({name}) => {
-  if (!useMediaQuery('(min-width:960px)')) 
-    name = formatService.setShortName(name) 
-  return <span>{name}</span>;
-}
-
 const useStyles = makeStyles((theme)=> ({
   root: {
     margin: 'auto',
@@ -41,12 +33,22 @@ const useStyles = makeStyles((theme)=> ({
     "&:hover": {
       backgroundColor: "#f9f9f9"
     }
+  },
+  nameField: {
+    whiteSpace: 'nowrap',
   }
 }));
+
 
 const ListOfPlans = ({ onPlanClicked, plans, setPlans, isHr }) => {
 
   const classes = useStyles();
+  
+  const NameField = ({name}) => {
+    if (!useMediaQuery('(min-width:960px)')) 
+      name = formatService.setShortName(name) 
+    return <span className={classes.nameField}>{name}</span>;
+  }
   const exposureService = useExpService();
 
   const deletePlan = (id, event) => {
@@ -72,12 +74,12 @@ const ListOfPlans = ({ onPlanClicked, plans, setPlans, isHr }) => {
             <TableBody>
             {plans.map(plan => (
               <TableRow className={classes.planRow} onClick={() => onPlanClicked(plan.id)} key={plan.id}>
-                <TableCell>
-                  <Name name={plan.employee.name}/>
+                <TableCell >
+                  <NameField name={plan.employee.name} />
                   <Typography variant="subtitle1">{plan.employeePosition.name}</Typography>
                 </TableCell>
                 <TableCell><Typography variant="subtitle2">{formatService.getStage(plan.stage)}</Typography></TableCell>
-                {isHr && <TableCell><Name name={plan.supervisor.name}/></TableCell>} 
+                {isHr && <TableCell><NameField  name={plan.supervisor.name}/></TableCell>} 
                 <TableCell>{formatService.setDate(plan.date)}</TableCell>
                 {isHr && 
                   <TableCell>
